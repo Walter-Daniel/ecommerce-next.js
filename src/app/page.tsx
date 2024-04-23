7170db8a2d526372233e77375d3bbd976db86390
Ulises <ulisescappa@gmail.com>
2024-04-22 21:13:53 -0300

    Create ProductCard and ProductList components. Add test images to test the cards.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 
 import { Button } from "@nextui-org/react";
 import Hero from "@/components/Hero";
+import ProductList from "@/components/ProductList";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <div>
+        <ProductList />
+      </div>
     </main>
   );
 }
