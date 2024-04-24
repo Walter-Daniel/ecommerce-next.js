5fddbd5b3819286a0fc29d0cf38d8a5464508820
Ulises <ulisescappa@gmail.com>
2024-04-24 03:24:20 -0300

    Add basic navbar filters to display men, women or unisex products.

--- src/components/NavbarSection.tsx
+++ src/components/NavbarSection.tsx
@@ -13,7 +13,7 @@ const NavbarSection = () => {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         {navList.map((navItem, index )=> (
           <NavbarItem key={index}>
-            <Link color="foreground" href="#" className='navbar__link relative'>
+            <Link color="foreground" href={`/products/${navItem.toLowerCase()}`} className='navbar__link relative'>
               {navItem}
             </Link>
           </NavbarItem>
