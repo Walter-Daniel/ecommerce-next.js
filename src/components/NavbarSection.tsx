9a38c4dd1d6d2b7d3890e19432094229edcd78eb
Ulises <ulisescappa@gmail.com>
2024-04-24 04:11:16 -0300

    Modify navbar links to show a menu clicking in the categories link.

--- src/components/NavbarSection.tsx
+++ src/components/NavbarSection.tsx
@@ -1,29 +1,73 @@
 'use client'
 
-import React from 'react'
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import React, { useState, useEffect, useRef } from 'react';
+import { Navbar, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
 
 const NavbarSection = () => {
+  const navList = ['HOME', 'CATEGORIES', 'MEN', 'WOMEN', 'HOT OFFERS'];
+  const categories = ['dress', 'footwear', 'accessories', 'shirt'];
 
-  const navList = ['HOME', 'CATEGORIES', 'MEN', 'WOMEN', 'HOT OFFERS' ]
+  const categoriesRef = useRef(null); // Referencia al menú de categorías
+
+  const [showCategoriesMenu, setShowCategoriesMenu] = useState(false);
+
+  // Handler de click para mostrar/ocultar el menú de categorías
+  const handleCategoriesClick = () => {
+    setShowCategoriesMenu(!showCategoriesMenu);
+  };
+
+  // Handler de click en cualquier parte de la pagina
+  const handleClickOutside = (event) => {
+    if (categoriesRef.current && !categoriesRef.current.contains(event.target)) {
+      setShowCategoriesMenu(false); // para cerrar el menu de categorias si se hace clic fuera de él
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
-    <Navbar shouldHideOnScroll className='justify-center' >
-    
+    <Navbar shouldHideOnScroll className='justify-center'>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {navList.map((navItem, index )=> (
+        {navList.map((navItem, index) => (
           <NavbarItem key={index}>
-            <Link color="foreground" href={`/products/${navItem.toLowerCase()}`} className='navbar__link relative'>
-              {navItem}
-            </Link>
-          </NavbarItem>
-        ) )}
-      </NavbarContent>
-      
-    </Navbar>
+            {navItem === 'CATEGORIES' ? (
+              
+              <div className="relative" ref={categoriesRef}>
+                <Link color="foreground" className="navbar__link relative" onClick={handleCategoriesClick}>
+                  {navItem}
+                </Link>
 
+                {/* Menú desplegable de categorías */}
+
+                {showCategoriesMenu && (
+                  <ul className="absolute left-0 mt-2 bg-white shadow-lg rounded-md">
+                    {categories.map((cate) => (
+                      <li className="py-2 px-4">
+                        <Link href={`/products/${cate}`} color="foreground" className="navbar__link relative">
+                          {cate.charAt(0).toUpperCase() + cate.slice(1)}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ) : (
     
-  )
-}
+              <Link color="foreground" href={`/products/${navItem.toLowerCase()}`} className='navbar__link relative'>
+                {navItem}
+              </Link>
+              
+            )}
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+    </Navbar>
+  );
+};
 
 export default NavbarSection;
