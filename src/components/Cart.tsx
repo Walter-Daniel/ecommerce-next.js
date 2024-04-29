f9fa0c88b97a07edcee9a275b00fcf935ad6e656
Micaela <micaelauvilla9@gmail.com>
2024-04-29 13:26:07 -0300

    cart finish and add skeleton

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,17 +1,15 @@
 "use client";
-import { CartContext } from "@/context/CartContext";
-import Link from "next/link";
-import React, { useContext, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "@/app/firebase";
 import { UserAuth } from "@/app/context/AuthProvider";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 interface CartProps {
   cartOpen: Boolean;
   setCartOpen: Function;
 }
- interface CartItemsProps {
+interface CartItemsProps {
   title: string;
   image: string;
   description: string;
@@ -24,6 +22,8 @@ const Cart: React.FC<CartProps> = ({ cartOpen, setCartOpen }) => {
   const router = useRouter();
   const { user } = UserAuth();
   const [cartItems, setCartItems] = useState<CartItemsProps[]>([]);
+
+
   
 
   useEffect(() => {
@@ -60,66 +60,63 @@ const Cart: React.FC<CartProps> = ({ cartOpen, setCartOpen }) => {
     0
   );
   const removeProductFromCart = (productId: string) => {
-
     const cartString = localStorage.getItem("cart");
-  
-  if (cartString) {
-    const cart = JSON.parse(cartString);
-    const updatedCart = cart.filter((product: any) => product.id !== productId);
-    localStorage.setItem("cart", JSON.stringify(updatedCart));
-  }
-};
+
+    if (cartString) {
+      const cart = JSON.parse(cartString);
+      const updatedCart = cart.filter(
+        (product: any) => product.id.toString() !== productId
+      );
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+    }
+  };
 
   const updateLocalStorage = (cartItems: CartItemsProps[]) => {
     localStorage.setItem("cart", JSON.stringify(cartItems));
   };
-  const shoppingFinish =async()=>{
-    if(!user){
-      alert("Necesita iniciar sesion primero")
-    }
 
+  
+  const shoppingFinish = async () => {
     await setDoc(doc(collection(db, `shop/cart-shop/${user!.uid}`)), {
       cartItems,
-      amount: totalPrice
+      amount: totalPrice,
     });
     localStorage.removeItem("cart");
-    alert("su compra se ha realizado con exito");
-    router.push('/')
-    setCartOpen(!cartOpen)
- 
-
-
-  }
+    alert("Your purchase has been made successfully");
+    router.push("/");
+    setCartOpen(!cartOpen);
+  };
   return (
     <div
       className={`fixed top-0 right-0 bottom-0 sm:w-3/4 md:w-1/2 xl:w-1/3   w-full bg-transparent z-50 transition-transform duration-300 ease-in-out justify-between text-black  ${
         cartOpen ? "transform translate-x-0 " : "transform translate-x-full"
       }`}
     >
-      <div className="bg-gray-100 py-4 px-6 overflow-y-auto max-h-[calc(100vh-0rem)] justify-between  ">
+      <div className="bg-gray-100 py-4 px-6 overflow-y-auto max-h-[calc(100vh-0rem)]   justify-between  ">
         <div className="flex items-center justify-between">
-          <h2 className="text-2xl font-bold   ">Carrito de Compras</h2>
+          <h2 className="text-2xl font-bold   ">Cart</h2>
           <RxCross1
             className="text-[20px]"
             onClick={() => setCartOpen(!cartOpen)}
           />
         </div>
 
-        <div className="bg-white rounded shadow md:pl-8 ">
+        <div className="bg-white rounded shadow md:pl-8   ">
           <div className="m-5 flex flex-col justify-between   ">
             {cartItems.length === 0 ? (
-              <div className="px-6 py-4 text-sm ">
-                No hay productos en el carrito
+              <div className="px-6 py-4 text-sm font-semibold ">
+                No products in cart yet.
               </div>
             ) : (
               cartItems.map((product: CartItemsProps, index: number) => (
                 <div
-                  key={index}
-                  className="flex  border-b-2 border-gray-200 justify-between"
+                key={index}
+                className="flex  border-b-2 border-gray-200 justify-between"
+                
                 >
-                  <div className="flex py-5 w-20 h-32 ">
+                  <div className="flex py-5 w-20 h-32 object-cover md:gap-10">
                     <img
-                      className=" rounded-full "
+                      className=" rounded-full  "
                       src={product.image}
                       alt={product.title}
                     />
@@ -146,7 +143,14 @@ const Cart: React.FC<CartProps> = ({ cartOpen, setCartOpen }) => {
                       </div>
                     </div>
                     <div className="flex flex-col items-end  text-sm md:ml-10  ">
-                      <button className="text-[#D90429]  " >Eliminar</button>
+                      <button
+                        className="text-[#D90429]  "
+                        onClick={() =>
+                          removeProductFromCart(product.id.toString())
+                        }
+                      >
+                       REMOVE
+                      </button>
 
                       <h3 className=" font-bold  ">${calculateTotal(index)}</h3>
                     </div>
@@ -158,11 +162,14 @@ const Cart: React.FC<CartProps> = ({ cartOpen, setCartOpen }) => {
         </div>
         <div className="flex flex-col justify-end items-end">
           <div className="mt-4 text-right items-end justify-end  text-xl font-bold">
-            Precio Total: ${totalPrice.toFixed(3)}
+            Total: ${totalPrice.toFixed(3)}
           </div>
           <div className="flex justify-end mt-4">
-            <button onClick={shoppingFinish}  className="px-4 text-sm py-2 border-1 border-[#D90429] bg-white  font-medium rounded-md ">
-              COMPRAR
+            <button
+              onClick={shoppingFinish}
+              className="px-4 text-sm py-2 border-1 border-[#D90429] bg-white  font-medium rounded-md "
+            >
+              BUY
             </button>
           </div>
         </div>
