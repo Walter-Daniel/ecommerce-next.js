be5f5ca54f422e0caf52e35206a7b888cfadb01d
Ulises <ulisescappa@gmail.com>
2024-04-29 16:06:16 -0300

    Fix navbar filter. Add searchbar functionality adding ProductSearch component to do that. Add utils functions to fetch data in the /utils/firebase.js file.

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -6,11 +6,20 @@ import { FiHeart } from "react-icons/fi";
 import { LuShoppingCart } from "react-icons/lu";
 import { CartProvider } from "../context/CartContext";
 import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import Cart from "./Cart";
+import router from "next/router";
 
 const HeaderMain = () => {
   const [cartOpen, setCartOpen] = useState(false);
+  const [searchBar, setSearchBar] = useState('')
+
+  const handleChange = (event) => {
+    setSearchBar(event.target.value);
+    
+  };
 
   const handleCart = () => {
     setCartOpen(!cartOpen);
@@ -19,8 +28,11 @@ const HeaderMain = () => {
   return (
     <div className="boder-b border-gray-200 py-6">
       <div className="container sm:flex justify-between items-center">
-        <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish">
-          <Image src="/logo.png" alt="Logo" width={200} height={100} />
+        <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish cursor-pointer">
+          <Link href="/">
+            <Image src="/logo.png" alt="Logo" width={200} height={100} />
+          </Link>
+          
         </div>
 
         <div className="w-full sm:w-[300px] md:w-[70%] relative">
@@ -28,11 +40,15 @@ const HeaderMain = () => {
             className="border-gray-200 border p-2 px-4 rounded-lg w-full"
             type="text"
             placeholder="Enter any product name..."
+            onChange={handleChange}
           />
-          <BsSearch
-            className="absolute right-0 top-0 mr-3 mt-3 text-gray-400"
-            size={20}
-          />
+          <Link href={`/search/${searchBar}`}>
+            <BsSearch
+              className="absolute right-0 top-0 mr-3 mt-3 text-gray-400 cursor-pointer"
+              size={20}
+            />
+          </Link>
+          
         </div>
 
         <div className=" lg:flex text-gray-500 text-[30px] gap-4">
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
-import { collection, doc, writeBatch,} from "firebase/firestore"
-import { firestore as db } from "@/app/firebase"
+import { collection, doc, writeBatch,query, where, getDocs} from "firebase/firestore"
+import { db } from "@/app/firebase"
 import products from "./data.json"
 
 
@@ -20,4 +20,55 @@ const addMultipleProducts = async () => {
     await batch.commit()
 }
 
-export default addMultipleProducts
+const filterByGenre = async (filter) => {
+    let genreFilter;
+    if (filter === "men") {
+      genreFilter = ["Men", "Unisex"];
+    } else if (filter === "women") {
+      genreFilter = ["Women", "Unisex"];
+    } else {
+      return [];
+    }
+  
+    const q = query(collection(db, "products"), where("genre", "in", genreFilter));
+    const productCollectionSnapshot = await getDocs(q);
+    const products = productCollectionSnapshot.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id
+    }));
+    return products;
+};
+
+const filterByCategory = async (filter) => {
+    const q = query(collection(db, "products"), where("category", "==", filter))
+    const productCollectionSnapshot = await getDocs(q)
+    const products = productCollectionSnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return products
+}
+
+const filterByOffers = async () => {
+    const q = query(collection(db, "products"), where("hot-offer", "==", true))
+    const productCollectionSnapshot = await getDocs(q)
+    const products = productCollectionSnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return products
+}
+
+const searchProduct = async (word) => {
+    const q = query(collection(db, "products"));
+    const productCollectionSnapshot = await getDocs(q);
+    const products = productCollectionSnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }));
+
+    return products;
+}
+
+
+export default { addMultipleProducts,filterByCategory, filterByOffers, filterByGenre, searchProduct }
