3c44df2435107d1eb238bcff875ea1fe3dbe131f
Walter <walterdcarrizo.19@gmail.com>
2024-04-29 21:29:59 -0300

    Links were added to go to the homepage, and the images were configured

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,6 +11,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FormAuth } from '@/components/FormAuth';
 import { Inputs } from '../types';
 import { ButtonForm } from '@/components/ButtonForm';
+import { FcAdvance } from 'react-icons/fc';
+
+import logo from '../../../public/logo.png'
 
 
 export default function RegisterPage() {
@@ -43,7 +46,11 @@ export default function RegisterPage() {
       <div className="lg:pr-44 w-[100%] h-[100%] flex justify-center lg:justify-end items-center p-40">
         <form onSubmit={handleSubmit(onSubmit)} className="bg-white bg-opacity-60 backdrop-filter backdrop-blur-lg mx-5 md:mx-0 p-5 md:p-10 w-[100%] md:w-2/3 lg:w-1/3 rounded-xl shadow-xl fade-in">
           <div className="flex justify-end mb-5">
-            <Image src='/logo.png' alt="Logo" width={200} height={200} style={{ width: 'auto', height:'auto' }}/>
+            <Image 
+              src={logo} 
+              alt="Logo" 
+              sizes='50px' 
+              style={{width: '100%', height: '50px', objectFit:'contain'}} />
           </div>
             {/* Name */}
             <div className="flex flex-col pb-5 relative" >
@@ -75,10 +82,16 @@ export default function RegisterPage() {
                   }
             </div>
             {/* BUTTONS */}
-            <ButtonForm handleGoogleSignIn={handleGoogleSignIn} />
+            <ButtonForm handleGoogleSignIn={handleGoogleSignIn} title='Sign up' />
             <div className="flex gap-2 pt-3">
               <p className='text-small'>Already have an account?</p>
               <Link href={'/login'} className="underline text-red-500 text-small">Login</Link>
+            </div>
+            <div className="flex gap-2 pt-3 justify-end items-center">
+              <FcAdvance />
+              <Link href={'/'} className="text-black text-sm">
+                Go home
+              </Link>
             </div>
         </form>
       </div>
